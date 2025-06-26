8ed1d9794670f4caec40115f77f58536eb7f4b7b
munir <smsirajulmonir@gmail.com>
2025-06-26 23:16:31 +0600

    deploy error fixed

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ app.use(express.json())
 
 app.use(
     cors({
-         origin: ['http://localhost:5173'],
+         origin: ['https://smartbrief-frontend.vercel.app'],
         credentials: true 
     }))
 
